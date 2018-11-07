04f679e7872416c6b32a90670b6dee913d44abc0
Gao Wang <gaow@uchicago.edu>
2018-11-07 17:31:38 -0600

    Add single effect regression test

---
+++ tests/testthat/test_single_effect_regression.R
@@ -0,0 +1,26 @@
+context("Test Single Effect regression")
+
+test_that("mmbr is identical to susieR", with(simulate_univariate(), {
+    # Test fixed prior
+    A = susieR:::single_effect_regression(y, X, V, residual_variance = 1, prior_weights = NULL, optimize_V = FALSE)
+    kl = susieR:::SER_posterior_e_loglik(X,y,1,A$alpha*A$mu,A$alpha*A$mu2)- A$loglik
+    B = SingleEffectRegression(BayesianMultipleRegression)$new(d$n_effect, 1, V, estimate_prior_variance = FALSE, prior_weights = NULL)
+    d.copy = d$clone(T)
+    B$fit(d.copy)
+    B$compute_kl(d.copy)
+    expect_equal(A$alpha * A$mu, B$posterior_b1)
+    expect_equal(A$alpha * A$mu2, B$posterior_b2)
+    expect_equal(A$lbf_model, B$lbf_single_effect)
+    expect_equal(kl, B$kl)
+    # Test estimated prior
+    A = susieR:::single_effect_regression(y, X, V, residual_variance = 1, prior_weights = NULL, optimize_V = TRUE)
+    kl = susieR:::SER_posterior_e_loglik(X,y,1,A$alpha*A$mu,A$alpha*A$mu2)- A$loglik
+    B = SingleEffectRegression(BayesianMultipleRegression)$new(d$n_effect, 1, V, estimate_prior_variance = TRUE, prior_weights = NULL)
+    d.copy = d$clone(T)
+    B$fit(d.copy)
+    B$compute_kl(d.copy)
+    expect_equal(A$alpha * A$mu, B$posterior_b1)
+    expect_equal(A$alpha * A$mu2, B$posterior_b2)
+    expect_equal(A$lbf_model, B$lbf_single_effect)
+    expect_equal(kl, B$kl)
+}))
