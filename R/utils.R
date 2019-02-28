9bcb38e67130d2ddbe03df7d372cb9462acc3c18
Gao Wang <gaow@uchicago.edu>
2019-02-28 10:25:17 -0600

    Add per condition PIP

--- R/utils.R
+++ R/utils.R
@@ -47,8 +47,17 @@ report_susie_model = function(d, m) {
 #' @title Compute condition specific posterior inclusion probability
 #' @param m M&M model
 #' @param prior_obj prior mixture object
+#' @export
 mmbr_get_pip_per_condition = function(m, prior_obj) {
-
+  condition_indicator = do.call(rbind, lapply(1:length(prior_obj$prior_covariance$xUlist), function(i) as.integer(diag(prior_obj$prior_covariance$xUlist[[i]]) != 0)))
+  condition_pip = array(0, dim=dim(m$mu))
+  for (r in 1:dim(condition_pip)[3]) {
+    for (p in 1:length(condition_indicator[,r])) {
+        condition_pip[,,r] = condition_pip[,,r] + m$mixture_weights[,,p] * condition_indicator[p,r]
+    }
+    condition_pip[,,r] = condition_pip[,,r] * m$alpha
+  }
+  return(do.call(cbind, lapply(1:dim(condition_pip)[3], function(r) apply(condition_pip[,,r], 2, function(x) 1-prod(1-x)))))
 }
 
 #' @title A null progressbar, because currently `progressbar_enabled` feature does not work for `progress_bar`
