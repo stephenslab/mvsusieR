1497ba595e5005a077402117b5d2db0cd7e370ba
Gao Wang <gaow@uchicago.edu>
2019-03-25 12:59:55 -0500

    Update optimize V method for Bayesian multiple regression

--- R/bayesian_multiple_regression.R
+++ R/bayesian_multiple_regression.R
@@ -107,6 +107,17 @@ lbf.grad = function(V,sbhat2,T2){
   return(l)
 }
 
+loglik = function(V,betahat,shat2,prior_weights) {
+  lbf = dnorm(betahat,0,sqrt(V+shat2),log=TRUE) - dnorm(betahat,0,sqrt(shat2),log=TRUE)
+  #log(bf) on each SNP
+  lbf[shat2==Inf] = 0 # deal with special case of infinite shat2 (eg happens if X does not vary)
+  maxlbf = max(lbf)
+  w = exp(lbf-maxlbf) # w =BF/BFmax
+  w_weighted = w * prior_weights
+  weighted_sum_w = sum(w_weighted)
+  return(log(weighted_sum_w)+ maxlbf)
+}
+
 loglik.grad = function(V,bhat,sbhat2,prior_weights) {
   #log(bf) on each effect
   lbf = dnorm(bhat,0,sqrt(V+sbhat2),log=TRUE) - dnorm(bhat,0,sqrt(sbhat2),log=TRUE)
@@ -115,21 +126,24 @@ loglik.grad = function(V,bhat,sbhat2,prior_weights) {
   sum(alpha*lbf.grad(V,sbhat2,bhat^2/sbhat2))
 }
 
-# define gradient as function of lV:=log(V)
-# to improve numerical optimization
-negloglik.grad.logscale = function(lV,bhat,sbhat2,prior_weights) {
-  -exp(lV)*loglik.grad(exp(lV),bhat,sbhat2,prior_weights)
+neg.loglik.logscale = function(lV,betahat,shat2,prior_weights){
+  return(-loglik(exp(lV),betahat,shat2,prior_weights))
 }
 
-est.prior.variance = function(bhat,sbhat2,prior_weights) {
-  if(loglik.grad(0,bhat,sbhat2,prior_weights)<0){
-    return(0)
+# define gradient as function of lV:=log(V)
+# to improve numerical optimization
+negloglik.grad.logscale = function(lV,betahat,shat2,prior_weights) {
+  -exp(lV)*loglik.grad(exp(lV),betahat,shat2,prior_weights)
+}
+
+est.prior.variance = function(betahat,shat2,prior_weights, method=c('optim', 'uniroot')) {
+  if(method=="optim"){
+    lV = optim(par=log(max(c(betahat^2-shat2, 1), na.rm = TRUE)), fn=neg.loglik.logscale, betahat=betahat, shat2=shat2, prior_weights = prior_weights, method='Brent', lower = -10, upper = 15)$par
+    V = exp(lV)
   } else {
-    ##V.o = optim(par=log(V),fn=negloglik.logscale,gr = negloglik.grad.logscale,bhat=bhat,sbhat2=sbhat2,prior_weights=prior_weights,method="BFGS")
-    ##if(V.o$convergence!=0){
-    ##  warning("optimization over prior variance failed to converge")
-    ##}
-    V.u=uniroot(negloglik.grad.logscale,c(-10,10),extendInt = "upX",bhat=bhat,sbhat2=sbhat2,prior_weights=prior_weights)
-    return(exp(V.u$root))
+    V.u = uniroot(negloglik.grad.logscale,c(-10,10),extendInt = "upX",betahat=betahat,shat2=shat2,prior_weights=prior_weights)
+    V = exp(V.u$root)
   }
+  if(loglik(0,betahat,shat2,prior_weights) >= loglik(V,betahat,shat2,prior_weights)) V=0 # set V exactly 0 if that beats the numerical value
+  return(V)
 }
