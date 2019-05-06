68cb89aa119501b1860c70e6d3740928b5a3838d
Gao Wang <gaow@uchicago.edu>
2019-05-05 22:21:54 -0500

    Fix is_common_v dimension issue

--- R/utils.R
+++ R/utils.R
@@ -101,7 +101,7 @@ null_progress_bar = R6Class('null_progress_bar', public = list(tick = function(.
 #' @title check if all elements are the same in matrix of J by R, J >> R
 #' @keywords internal
 is_mat_common = function(mat) {
-  sum(1-duplicated(mat, MARGIN=2)) == 1
+  sum(1-duplicated(mat, MARGIN=1)) == 1
 }
 
 #' @title A simple simulation function to simulate some test data
