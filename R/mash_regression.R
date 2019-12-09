696a547263fc6dd1dd5cd3db5d2ab98629442810
Gao Wang <gaow@uchicago.edu>
2019-12-09 14:18:38 -0600

    Add 'simple' method to estimate prior variance for MashRegrssion

--- R/mash_regression.R
+++ R/mash_regression.R
@@ -17,10 +17,11 @@ MashRegression <- R6Class("MashRegression",
         warning(paste0('Cannot compute inverse for residual variance due to error:\n', e, '\nELBO computation will thus be skipped.'))
       })
       if (is.matrix(residual_variance)) private$residual_correlation = cov2cor(residual_variance)
-      else private$residual_correlation = diag(1) 
+      else private$residual_correlation = diag(1)
       private$alpha = mash_initializer$alpha
       private$precomputed_cov_matrices = mash_initializer$precomputed
       private$.posterior_b1 = matrix(0, J, mash_initializer$n_condition)
+      private$prior_variance_scale = 1
     },
     fit = function(d, prior_weights = NULL, use_residual = FALSE, save_summary_stats = FALSE, estimate_prior_variance_method = NULL) {
       # d: data object
@@ -81,26 +82,47 @@ MashRegression <- R6Class("MashRegression",
       # 1.3 get relative loglik
       lfactors = apply(llik,1,max)
       llik = list(loglik_matrix=llik-lfactors, lfactors=lfactors)
-      # 2. compute posterior weights
+      # 2. lbf
+      lbf_obj = private$compute_lbf(llik, s_alpha)
+      private$.lbf = lbf_obj$lbf
+      private$.loglik_null = lbf_obj$loglik_null
+      if (!is.null(estimate_prior_variance_method)) {
+        if (estimate_prior_variance_method != 'simple')
+          stop(paste("Estimate prior method", estimate_prior_variance_method, "is not available for MashRegression."))
+        if (is.null(prior_weights))
+          prior_weights = rep(1/private$J, private$J)
+        # the commented out line is consistent with comparing loglik for V=0 vs current V
+        # but is more agressive than checking if all `private$.lbf` are smaller than zero.
+        #if (compute_weighted_sum(private$.lbf, prior_weights)$log_sum <= 0) {
+        if (all(private$.lbf <= 0)) {
+          private$prior_variance_scale = 0
+          private$.lbf = private$.lbf * 0
+        }
+      }
+      # 3. compute posterior weights
       private$.mixture_posterior_weights = mashr:::compute_posterior_weights(private$.prior_variance$pi, exp(llik$loglik_matrix))
-      # 3. posterior
+      # 4. posterior
       ## FIXME: we might not need to compute second moment at all if we do not need to estimate residual variance
       ## we can get away with checking for convergence by PIP not by ELBO
       ## but let's set report_type = 4 and compute posterior covariance for now
-      if (is.null(private$precomputed_cov_matrices) || ncol(bhat) == 1) {
-        post = mashr:::calc_post_rcpp(t(bhat), t(sbhat), t(s_alpha), matrix(0,0,0), 
+      if (is.null(private$precomputed_cov_matrices) || ncol(bhat) == 1 || private$prior_variance_scale != 1) {
+        if (private$prior_variance_scale != 1)
+          xUlist = private$.prior_variance$xUlist * private$prior_variance_scale
+        else
+          xUlist = private$.prior_variance$xUlist
+        post = mashr:::calc_post_rcpp(t(bhat), t(sbhat), t(s_alpha), matrix(0,0,0),
                               private$residual_correlation,
-                              matrix(0,0,0), matrix(0,0,0), 
-                              private$.prior_variance$xUlist,
+                              matrix(0,0,0), matrix(0,0,0),
+                              xUlist,
                               t(private$.mixture_posterior_weights),
                               is_common_cov, 4)
       } else {
-        post = mashr:::calc_post_precision_rcpp(t(bhat), t(sbhat), t(s_alpha), matrix(0,0,0), 
+        post = mashr:::calc_post_precision_rcpp(t(bhat), t(sbhat), t(s_alpha), matrix(0,0,0),
                               private$residual_correlation,
-                              matrix(0,0,0), matrix(0,0,0), 
+                              matrix(0,0,0), matrix(0,0,0),
                               private$precomputed_cov_matrices$Vinv,
                               private$precomputed_cov_matrices$U0,
-                              t(private$.mixture_posterior_weights), 
+                              t(private$.mixture_posterior_weights),
                               is_common_cov, 4)
       }
       private$.posterior_b1 = post$post_mean
@@ -110,18 +132,8 @@ MashRegression <- R6Class("MashRegression",
       #  post$post_cov = array(post$post_cov, c(1, 1, private$J))
       #}
       private$.posterior_b2 = post$post_cov + simplify2array(lapply(1:nrow(post$post_mean), function(i) tcrossprod(post$post_mean[i,])))
-      # 4. lfsr
+      # 5. lfsr
       private$.lfsr = compute_lfsr(post$post_neg, post$post_zero)
-      # 5. lbf
-      # using mashr functions have to ensure s_alpha has valid log and rowSums
-      if (nrow(s_alpha) == 0) s_alpha = matrix(1,1,1)
-      private$.loglik_null = mashr:::compute_null_loglik_from_matrix(llik, s_alpha)
-      loglik_alt = mashr:::compute_alt_loglik_from_matrix_and_pi(private$.prior_variance$pi, llik, s_alpha)
-      private$.lbf = loglik_alt - private$.loglik_null
-      if (!is.null(ncol(private$.lbf)) && ncol(private$.lbf) == 1)
-        private$.lbf = as.vector(private$.lbf)
-      # Inf - Inf above can cause NaN
-      private$.lbf[which(is.na(private$.lbf))] = 0
     }
   ),
   active = list(
@@ -138,10 +150,23 @@ MashRegression <- R6Class("MashRegression",
     residual_correlation = NULL,
     precomputed_cov_matrices = NULL,
     alpha = NULL,
+    prior_variance_scale = NULL,
     .mixture_posterior_weights = NULL,
     .lfsr = NULL,
-    .residual_variance_inv = NULL
-  )
+    .residual_variance_inv = NULL,
+    compute_lbf = function(llik, s) {
+      # using mashr functions have to ensure s_alpha has valid log and rowSums
+      if (nrow(s) == 0) s = matrix(1,1,1)
+        loglik_null = mashr:::compute_null_loglik_from_matrix(llik, s)
+        loglik_alt = mashr:::compute_alt_loglik_from_matrix_and_pi(private$.prior_variance$pi, llik, s)
+        lbf = loglik_alt - loglik_null
+        if (!is.null(ncol(lbf)) && ncol(lbf) == 1)
+          lbf = as.vector(lbf)
+        # Inf - Inf above can cause NaN
+        lbf[which(is.na(lbf))] = 0
+        return(list(lbf=lbf, loglik_null=loglik_null))
+    }
+  ),
 )
 
 #' @title MASH initializer object
