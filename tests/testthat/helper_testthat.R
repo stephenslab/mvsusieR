195281c4454630c956406304a9907c1c17997321
zouyuxin <yuxin.zou.stat@gmail.com>
2020-01-07 15:34:50 -0600

    Add RSS version

--- tests/testthat/helper_testthat.R
+++ tests/testthat/helper_testthat.R
@@ -7,7 +7,7 @@ create_sparsity_mat = function(sparsity, n, p){
   return(mat)
 }
 
-simulate_univariate = function(n=100, p=200, sparse=F) {
+simulate_univariate = function(n=100, p=200, sparse=F, summary = F) {
   set.seed(1)
   beta = rep(0,p)
   beta[1:4] = 1
@@ -24,6 +24,11 @@ simulate_univariate = function(n=100, p=200, sparse=F) {
   scaled_prior_variance = 0.2
   X = susieR:::set_X_attributes(X)
   y = y - mean(y)
+  if(summary){
+    sumstats = susieR:::univariate_regression(X,y)
+    z = sumstats$betahat/sumstats$sebetahat
+    R = cor(X)
+  }
   s = list(alpha=matrix(1/p,nrow=L,ncol=p),
            mu=matrix(2,nrow=L,ncol=p),
            mu2=matrix(3,nrow=L,ncol=p),
@@ -33,10 +38,17 @@ simulate_univariate = function(n=100, p=200, sparse=F) {
   if (sparse) {
     # FIXME: sparse data not supported
     data = NA
-  } else {
+  } else if(summary){
+    data = RSSData$new(z, R, 1e-08)
+  }else {
     data = DenseData$new(X,y,TRUE,TRUE)
   }
-  return(list(X=X, X.sparse=X.sparse, s=s, d=data, y=y, n=n, p=p, V=s$V, b=beta, L=L))
+  if(summary){
+    return(list(X=X, X.sparse=X.sparse, z = z, R = R, s=s, d=data, y=y, n=n, p=p, V=s$V, b=beta, L=L))
+  }else{
+    return(list(X=X, X.sparse=X.sparse, s=s, d=data, y=y, n=n, p=p, V=s$V, b=beta, L=L))
+  }
+  
 }
 
 simulate_multivariate = function(n=100,p=100,r=2,center_scale=TRUE,y_missing=0) {
@@ -58,7 +70,7 @@ expect_susieR_equal = function(A, BA, estimate_prior_variance = FALSE, estimate_
   expect_equal(A$alpha * A$mu, BA$b1, tolerance = tol)
   expect_equal(A$alpha * A$mu2, BA$b2, tolerance = tol)
   if (!is.na(A$elbo) && !is.na(BA$elbo)) expect_equal(A$elbo, BA$elbo, tolerance = tol)
-  expect_equal(A$fitted, as.vector(BA$fitted), tolerance = tol)
+  expect_equal(as.vector(A$fitted), as.vector(BA$fitted), tolerance = tol)
   expect_equal(coef(A), BA$coef, tolerance = tol)
   if (estimate_residual_variance) expect_equal(A$sigma2, BA$sigma2, tolerance = tol)
   if (estimate_prior_variance) expect_equal(A$V, BA$V, tolerance = tol)
