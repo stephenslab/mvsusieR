9efadec715e847a0691b33d713e9b69d318b4484
Gao Wang <wangow@gmail.com>
2020-05-26 17:51:23 -0400

    Add functions to remove duplicated columns and to reconstruct input matrix based on duplication pattern learned #31

--- R/utils.R
+++ R/utils.R
@@ -174,6 +174,88 @@ is_list_common = function(lst) {
   length(almost.unique(lst)) == 1
 }
 
+#' @title remove duplicated columns in matrix while keeping track of what columns are removed for duplicate with what other column
+#' @keywords internal
+rm_collinear = function(mat, ...) {
+    # `duplicated` will only work for matrix not data frame
+    mat = as.matrix(mat)
+    dimmat = dim(mat)
+    bool_coll = almost.duplicated(mat, MARGIN = 2, ...)
+    if (any(bool_coll)) {
+        # these are columns to be removed
+        rmvd_coll = which(bool_coll)
+        # now find columns they are collinear with
+        # the idea is, when using fromLast, the previously NOT duplicated columns (FALSE) will now become duplicated (TRUE)
+        # then we can find these columns and use them as the columns that has some duplicated associated with them
+        bool_with_coll = almost.duplicated(mat, MARGIN = 2, fromLast = TRUE, ...) & !bool_coll
+        mat_with_coll = mat[, bool_with_coll, drop = FALSE]
+        mat_coll = mat[, bool_coll, drop = FALSE]
+        # these are columns with which the removed columns are collinear with
+        # `match` will only work for data frames
+        assoc_coll = which(bool_with_coll)[match(data.frame(mat_coll), data.frame(mat_with_coll))]
+        rmvd_coll = cbind(assoc_coll, rmvd_coll)
+        colnames(rmvd_coll) = c('associated', 'removed')
+        mat = mat[, !bool_coll, drop = FALSE]
+        # now generate index to recover the original
+        # ...
+    } else {
+        rmvd_coll = NULL
+    }
+    attr(mat, "original_dim") = dimmat
+    attr(mat, "collinear_cols") = rmvd_coll
+    if (is.null(rmvd_coll)) {
+        attr(mat, "collinear_counts") = NULL
+    } else {
+        attr(mat, "collinear_counts") = table(rmvd_coll[,'associated'])
+        attr(mat, "collinear_counts") = cbind(names(attr(mat, "collinear_counts")), attr(mat, "collinear_counts") + 1)
+        colnames(attr(mat, "collinear_counts")) = c('associated', 'counts')
+        rownames(attr(mat, "collinear_counts")) = NULL
+    }
+    return(mat)
+}
+
+#' @title Reconstruct complete matrix (with duplicates) using stripped matrix and information regarding duplicate pattern in original matrix
+#' @keywords internal
+reconstruct_coll = function(mat, coll_cols, coll_counts, original_dim, adjust_counts=FALSE, transpose=FALSE) {
+    # usage:
+    # m = rm_collinear(X1)
+    # X2 = reconstruct_coll(m, attr(m, 'collinear_cols'), attr(m, 'collinear_counts'), attr(m, 'original_dim'))
+    # sum(X1 - X2) == 0
+    get_count = function(idx) {
+        if (!adjust_counts) {
+            return (1)
+        } else {
+            return (coll_counts[,"counts"][which(coll_counts[,"associated"] == idx)])
+        }
+    }
+    vectorise = FALSE
+    if (is.vector(mat)) {
+        vectorise = TRUE
+        mat = matrix(mat, ncol = length(mat), nrow = 1)
+    }
+    if (transpose && !vectorise) mat = t(mat)
+    # create empty matrix to the original scale
+    res = matrix(NA, original_dim[1], original_dim[2])
+    # first column should always be good,
+    # and also duplicated columns always can be found in columns already established
+    res[,1] = mat[,1] / get_count(1)
+    i = 2
+    for (j in 2:ncol(res)) {
+        if (j %in% coll_cols[,'removed']) {
+            # a duplicate column, just add it from before
+            j0 = coll_cols[,'associated'][which(coll_cols[,'removed'] == j)]
+            res[,j] = res[,j0]
+        } else {
+            # a new column; have to take it from the next inline in input mat
+            res[,j] = mat[,i] / get_count(j)
+            i = i + 1
+        }
+    }
+    if (transpose && !vectorise) res = t(res)
+    if (vectorise) res = as.vector(res)
+    return(res)
+}
+
 #' @title A simple simulation function to simulate some test data
 #' @param n number of samples
 #' @param p number of features
