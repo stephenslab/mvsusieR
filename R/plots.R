253dbbefa1e70388c88fc28110fca736ff7114b6
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-11-19 15:04:04 -0600

    Added example for mvsusie_plot; also added some argument checking for mvsusie_plot.

--- R/plots.R
+++ R/plots.R
@@ -1,7 +1,11 @@
 #' @title Make Bubble Heatmap to Display mvsusie Result
 #' 
 #' @param m A mvsusie fit, typically the result of calling
-#'   \code{\link{mvsusie}}.
+#'   \code{\link{mvsusie}}. This function only works for a mvsusie model
+#'   fitted to multivariate response data (i.e., Y with more than one
+#'   column). This function only works for an mvsusie fit for
+#'   multivariate response and mash prior; mvsusie fits not satisfying
+#'   these conditions will result in an error.
 #'
 #' @param weighted_effect Describe input argument "weighted_effect"
 #'   here.
@@ -22,10 +26,31 @@
 #'   here.
 #' 
 #' @return A ggplot object.
-#' 
+#'
+#' @examples
+#' n = 500
+#' p = 1000
+#' true_eff = 2
+#' X = matrix(sample(0:2,size = n*p,replace = TRUE),nrow = n,ncol = p)
+#' beta1 = rep(0,p)
+#' beta2 = rep(0,p)
+#' beta3 = rep(0,p)
+#' beta1[1:true_eff] = runif(true_eff)
+#' beta2[1:true_eff] = runif(true_eff)
+#' beta3[1:true_eff] = runif(true_eff)
+#' y1 = X %*% beta1 + rnorm(n)
+#' y2 = X %*% beta2 + rnorm(n)
+#' y3 = X %*% beta3 + rnorm(n)
+#' Y = cbind(y1,y2,y3)
+#' prior =
+#'   create_mash_prior(sample_data = list(X=X,Y=Y,residual_variance=cov(Y)),
+#'                     max_mixture_len = -1)
+#' fit = mvsusie(X,Y,prior_variance = prior,compute_univariate_zscore = TRUE)
+#' mvsusie_plot(fit,plot_z = FALSE)$plot
+#' mvsusie_plot(fit,plot_z = TRUE)$plot
+#'
 #' @importFrom stats pnorm
 #' @importFrom ggplot2 ggplot
-#' @importFrom ggplot2 aes
 #' @importFrom ggplot2 aes_string
 #' @importFrom ggplot2 geom_point
 #' @importFrom ggplot2 scale_x_discrete
@@ -40,12 +65,11 @@
 #' @importFrom ggplot2 element_text
 #' @importFrom ggplot2 element_blank
 #' @importFrom ggplot2 geom_rect
-#' 
+#'
 #' @export
 #' 
 mvsusie_plot = function (m, weighted_effect = FALSE, cs_only = TRUE,
-                         plot_z = FALSE, pos = NULL,
-                         cslfsr_threshold = 0.05) {
+                         plot_z = FALSE, pos = NULL, cslfsr_threshold = 0.05) {
   colors = c(
     "dodgerblue2",
     "green4",
@@ -70,6 +94,13 @@ mvsusie_plot = function (m, weighted_effect = FALSE, cs_only = TRUE,
     "yellow3",
     "darkorange4",
     "brown")
+
+  # The susie fit should be for multivariate Y with mash prior.
+  if (!inherits(m,"susie"))
+    stop("Input argument \"m\" should be a susie fit object, such as the ",
+         "output of calling function \"mvsusie\"")
+  # if (!is.matrix(coef))
+  
   if (plot_z) {
     if (!("z" %in% names(m)))
       stop("Cannot find the z-score summary statistics")
@@ -110,61 +141,69 @@ mvsusie_plot = function (m, weighted_effect = FALSE, cs_only = TRUE,
   
   # Add CS to this table.
   if (!is.null(m$sets$cs_index)) {
-    if(plot_z == FALSE)
+    if(!plot_z)
       effects = as.vector(t(effects))
     j = 1
     for (i in m$sets$cs_index) {
       condition_idx = which(m$single_effect_lfsr[i,] < cslfsr_threshold)
       condition_sig = y_names[condition_idx]
       variables = x_names[m$sets$cs[[j]]]
-      table[which(table$x %in% variables),]$cs = i
+      table[which(table$x %in% variables),]$cs    = i
       table[which(table$x %in% variables),]$color = colors[i %% length(colors)]
-      if(plot_z == FALSE){
-        table[which(table$x %in% variables),]$mlog10lfsr = rep(-log10(pmax(1e-20, m$single_effect_lfsr[i,])),
-                                                               length(variables))
+      if (!plot_z) {
+        table[which(table$x %in% variables),]$mlog10lfsr =
+          rep(-log10(pmax(1e-20, m$single_effect_lfsr[i,])),length(variables))
         idx = which((table$x %in% variables) & (table$y %in% condition_sig))
         table[idx,]$effect_size = effects[idx]
       }
       j = j + 1
     }
-    if (cs_only){
+    if (cs_only)
       table = table[which(!is.na(table$cs)),]
-    }
   }
   
-  rowidx = which(table$x %in% x_names[pos])
-  table = table[rowidx,]
-  
-  cs_colors = unique(cbind(table$x, table$cs, table$color))[,3]
+  rowidx    = which(table$x %in% x_names[pos])
+  table     = table[rowidx,]
+  cs_colors = unique(cbind(table$x,table$cs,table$color))[,3]
   
   p = ggplot(table) +
     geom_point(aes_string(x = "x",y = "y",color = "effect_size",
                           size = "mlog10lfsr")) +
     scale_x_discrete(limits = unique(table$x)) +
     scale_y_discrete(limits = unique(table$y)) + 
-    scale_color_gradient2(midpoint = 0, limit = c(-max(abs(table$effect_size), na.rm=TRUE), 
-                                                  max(abs(table$effect_size), na.rm=TRUE)),
-                          low="#4575B4", mid="#FFFF0E", high="#D73027", space="Lab", na.value="white") +
-    labs(size=paste0("-log10(", ifelse(plot_z, "p", "CS lfsr"), ")"), colour=ifelse(plot_z, "z-score", "Effect size")) +
-    guides(size = guide_legend(order = 1), colour = guide_colorbar(order = 2)) +
-    theme_minimal() + theme(text = element_text(face = "bold", size = 14), panel.grid = element_blank(),
-                            axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1, size = 15, color = cs_colors),
-                            axis.text.y = element_text(size = 15, color = "black"),
-                            axis.title.x = element_blank(),
-                            axis.title.y = element_blank())
+    scale_color_gradient2(midpoint = 0,
+                          limit = c(-max(abs(table$effect_size),na.rm = TRUE), 
+                                    +max(abs(table$effect_size),na.rm = TRUE)),
+                          low = "#4575B4",mid =  "#FFFF0E",high = "#D73027",
+                          space = "Lab",na.value = "white") +
+    labs(size = paste0("-log10(",ifelse(plot_z,"p","CS lfsr"),")"),
+         colour = ifelse(plot_z,"z-score","effect size")) +
+    guides(size  = guide_legend(order = 1),
+           color = guide_colorbar(order = 2)) +
+    theme_minimal() +
+    theme(text = element_text(face = "bold",size = 14),
+          panel.grid   = element_blank(),
+          axis.text.x  = element_text(angle = 45,vjust = 1,hjust = 1,size = 15,
+                                      color = cs_colors),
+          axis.text.y  = element_text(size = 15,color = "black"),
+          axis.title.x = element_blank(),
+          axis.title.y = element_blank())
   
-  if (!is.null(top_snp)) {
-    if(top_snp %in% unlist(m$sets$cs)){
+  if (!is.null(top_snp))
+    if (top_snp %in% unlist(m$sets$cs)) {
       xnode = which(unique(table$x) == x_names[top_snp])
-      
-      p = p + geom_rect(aes(ymin=1-0.5,
-                            ymax=length(unique(table$y)) + 0.5,
-                            xmin=xnode-0.5,
-                            xmax=xnode+0.5), color="black", alpha=0, fill = "white")
+      p = p + geom_rect(data = data.frame(ymin = 0.5,
+                                          ymax = length(unique(table$y)) + 0.5,
+                                          xmin = xnode - 0.5,
+                                          xmax = xnode + 0.5),
+                        mapping = aes_string(ymin = "ymin",
+                                             ymax = "ymax",
+                                             xmin = "xmin",
+                                             xmax = "xmax"),
+                        color = "black",fill = "white",alpha = 0)
     }
-  }
-  w = length(unique(table$x)) * 0.6 + 3
-  h = length(unique(table$y)) * 0.7
-  cat(paste("Suggested PDF canvas width:", w, "height:", h, "\n"))
-  return(list(plot=p, width=w, height=h))
+  w = 3 + 0.6*length(unique(table$x))
+  h = 0.7*length(unique(table$y))
+  cat("Suggested PDF canvas width:",w,"height:",h,"\n")
+  return(list(plot = p,width = w,height = h))
 }
